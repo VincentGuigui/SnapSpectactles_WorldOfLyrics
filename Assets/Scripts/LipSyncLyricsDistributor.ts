31206bb0d002acbb43487f8344abf94f4b47716e
vincentguigui <vincent@guigui.fr>
2025-12-08 19:41:20 +0100

    - People are lip syncing too !!!

--- Assets/Scripts/LipSyncLyricsDistributor.ts
+++ Assets/Scripts/LipSyncLyricsDistributor.ts
@@ -5,6 +5,7 @@ import { LyricsData } from './LyricsData';
 @component
 export class LipSyncLyricsDistributor extends WordsLyricsDistributor {
 
+    pass: Pass = null;
     animationProvider: AnimatedTextureFileProvider = null;
 
     @input
@@ -14,18 +15,21 @@ export class LipSyncLyricsDistributor extends WordsLyricsDistributor {
 
     onStart() {
         super.onStart()
-        var pass = this.sceneObject.getComponent("Component.Image").mainPass
-        if (pass.baseTex) {
-            this.animationProvider = pass.baseTex.control as AnimatedTextureFileProvider;
-            if (this.animationProvider.isOfType('Provider.AnimatedTextureFileProvider')) {
-                this.animationProvider.pauseAtFrame(0)
-                if (this.autoPlay)
-                    this.animationProvider.playFromFrame(0, this.loop)
+        this.pass = this.sceneObject.getComponent("Component.Image").mainPass
+        if (this.pass.baseTex) {
+            this.animationProvider = this.pass.baseTex.control as AnimatedTextureFileProvider;
+            if (this.animationProvider != null) {
+                if (this.animationProvider.isOfType('Provider.AnimatedTextureFileProvider')) {
+                    this.animationProvider.pauseAtFrame(0)
+                    if (this.autoPlay)
+                        this.animationProvider.playFromFrame(0, this.loop)
+                }
             }
         }
     }
 
     setLyrics(lyrics: LyricsData, current: number, template: Text): void {
+        if (this.animationProvider == null) return
         if (current == LYRICS_STOP || current == LYRICS_PAUSE)
             this.animationProvider.stop()
         else
